cea882508a90a13badf282c61fb0de909d731e08
Ashrit Kulkarni <49915656+ashritkulkarni@users.noreply.github.com>
2021-09-26 21:52:25 -0400

    Delete embulk_to_pg directory

--- embulk_to_pg/queries/count_totalpageviews.sql
+++
@@ -1,9 +0,0 @@
-SELECT COUNT(url) AS total_pageviews 
-FROM pageviews
-WHERE user_id
-IN (
-  SELECT user_id
-  FROM customers
-  WHERE operating_system = 'Windows'
-  OR job_title LIKE '%Engineer%'
-  )
--- embulk_to_pg/queries/create_pageviews.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM pageviews_tmp
-WHERE user_id
-IN (SELECT user_id
-    FROM customers_tmp
-    WHERE job_title NOT LIKE '%Sales%');
