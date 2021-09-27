8bfbc38ab0cde1504c715a3972cb5ef99790c025
Ashrit Kulkarni <49915656+ashritkulkarni@users.noreply.github.com>
2021-09-26 21:52:03 -0400

    Add files via upload

---
+++ dd_embulk_pg/queries/top3_users.sql
@@ -0,0 +1,21 @@
+WITH maxpage_view AS (
+	SELECT user_id, max(timestamp) last_timestamp
+	FROM pageviews
+	WHERE user_id
+    IN (
+        SELECT user_id
+        FROM pageviews
+        WHERE url LIKE '%.gov%'
+        )
+	GROUP BY user_id
+	ORDER BY COUNT(url) DESC
+    LIMIT 3)
+SELECT user_id, url last_page_viewed
+FROM pageviews
+WHERE user_id
+IN (
+    SELECT user_id
+	FROM maxpage_view
+	WHERE timestamp=last_timestamp
+    )
+ORDER BY timestamp DESC;
